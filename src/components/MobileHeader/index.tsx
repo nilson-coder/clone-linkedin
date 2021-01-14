1f0e4aec3d2b9dd49bcf25244aa1c75378b472e9
nilson_pontes <nilson.pontes@arista.com.br>
2021-01-14 17:40:58 -0300

    1 commit

---
+++ src/components/MobileHeader/index.tsx
@@ -0,0 +1,16 @@
+// RFC
+import React from 'react';
+
+import { Container, ProfileCircle, SearchInput, MessageIcon } from './styles';
+
+const MobileHeader: React.FC = () => {
+  return (
+    <Container>
+      <ProfileCircle src ="https://github.com/guilhermerodz.png" />
+      <SearchInput placeholder="Pesquisar"/>
+      <MessageIcon />
+    </Container>
+  );
+};
+
+export default MobileHeader;
